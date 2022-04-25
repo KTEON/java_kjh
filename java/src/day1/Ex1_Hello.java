6d92553af48bdec981bf758f01ebe1eab25ff6ee
KTEON <104331435+KTEON@users.noreply.github.com>
2022-04-25 13:57:34 +0900

    자바 1일차 : Hello World 출력 예제

---
+++ java/src/day1/Ex1_Hello.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class Ex1_Hello {
+	// 콘솔 응용 프로그램이 실행되기 위해서 main이라는 애(메소드)가 필요하다
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*여러 줄 주석
+		 * 
+		 * 
+		 */
+		// 한 줄 주석은 아무 위치에서 사용가능하지만 중간은 안됨
+		/*println(문자열) : 문자열을 콘솔에 출력한 후 줄바꿈을 함(엔터 효과)
+		 * print(문자열) : 문자열을 콘솔에 출력
+		 */
+		System.out.print("Hello world!!");
+		System.out.println("Hello warld!!");
+		System.out.print("Hello warld");
+	}
+	
+}

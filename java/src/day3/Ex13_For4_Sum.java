fcb95c439653d0ff1245fb3d091a9b243c02d611
KTEON <104331435+KTEON@users.noreply.github.com>
2022-04-28 01:18:20 +0900

    자바 3일차 : for문_누적 합 출력 예제

---
+++ java/src/day3/Ex13_For4_Sum.java
@@ -0,0 +1,39 @@
+package day3;
+
+public class Ex13_For4_Sum {
+
+	public static void main(String[] args) {
+		/*
+		 * 1부터 5까지 합을 구하는 코드 작성
+		 */
+		int i;
+		int sum=0;
+		for(i=1;i<=5;i++) {
+			sum += i;
+			/* 		sum0=0
+			 * i=1	sum=0+1
+			 * i=2	sum=0+1+2
+			 * i=3	sum=0+1+2+3
+			 * i=4	sum=0+1+2+3+4
+			 * i=5	sum=0+1+2+3+4+5
+			 * 
+			 * 
+			 * 
+			 */
+			/* 		sum0=0
+			 * i=1	sum1=0+1
+			 * i=2	sum2=sum1+2
+			 * i=3	sum3=sum2+3
+			 * i=4	sum4=sum3+4
+			 * i=5	sum5=sum4+5
+			 * 
+			 * 
+			 * 
+			 */
+		}
+			System.out.println("1부터 5까지의 합 : " + sum);
+			
+		}
+	}
+
+

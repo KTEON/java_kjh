dae86ee28f594c649deea2b8116a02cd58feb5f3
KTEON <104331435+KTEON@users.noreply.github.com>
2022-04-28 01:20:18 +0900

    자바 3일차 : 중첩 If문_ 2,3,6의 배수 예제

---
+++ java/src/day3/Ex6_If3.java
@@ -0,0 +1,23 @@
+package day3;
+
+public class Ex6_If3 {
+
+	public static void main(String[] args) {
+		
+
+int num = 6;
+
+if (num%2 == 0) {
+	if(num % 3 == 0) {
+		System.out.println(num+ "는 6의 배수");
+	}else {
+		System.out.println(num + "는 2의배수");
+	}
+
+	}else if(num % 3 == 0) {
+		System.out.println(num + " 는 3의 배수");
+	}else {
+		System.out.println(num + "는 2, 3, 6의 배수가 아님");
+	}
+}
+}

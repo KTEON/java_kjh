4bbd89e5b0b206683f63f1753e0f8a06ac67e5e7
KTEON <104331435+KTEON@users.noreply.github.com>
2022-04-28 23:46:30 +0900

    자바 4일차 : while문 예제 - Hello 예제

---
+++ java/src/day4/Ex9_While1_Hello.java
@@ -0,0 +1,21 @@
+package day4;
+
+public class Ex9_While1_Hello {
+
+	public static void main(String[] args) {
+		/*
+		 *hello을 5번 출력 
+		 */
+		int i =1;
+		while(i<=5) {
+			System.out.println("Hello");
+			i++;
+		}
+		i=5;
+		while(i-->0) {
+			System.out.println("Hi");
+		}
+	}
+	
+
+}

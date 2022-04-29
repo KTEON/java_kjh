57e7e40e8f4104df9d18a710a8589316432a929b
KTEON <104331435+KTEON@users.noreply.github.com>
2022-04-29 17:07:47 +0900

    자바 5일차 : 반복문 예제 - 정수 역순출력예제

--- java/src/day5/Ex1_reverseNumber.java
+++ java/src/day5/Ex1_reverseNumber.java
@@ -1,5 +1,7 @@
 package day5;
 
+import java.util.Scanner;
+
 public class Ex1_reverseNumber {
 
 	public static void main(String[] args) {
@@ -19,6 +21,25 @@ public class Ex1_reverseNumber {
 		 * 규칙성 : num의 일의 자리를 출력 한 후 num를 10으로 나눈 몫을 num에 저장
 		 * 
 		 */
+		
+		
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		int reverseNum = 0;
+		int tmpNum = num;
+		while(num != 0) {
+			//num-num/10*10;---정수/정수 해서 숫자 자르고 곱하기 해서 123에서 120만드는 거 가능
+			System.out.println(num%10);
+			//0 ->0*10+4=4 -> 4*10+3= 43 -> 432 -> 4321
+			reverseNum = reverseNum + num %10;
+			num = num/10;
+		}
+		System.out.println();
+		System.out.println(tmpNum + "를 거꾸로한 정수 : " + reverseNum);
+		
+		
+		scan.close();
+		
 		
 	}
 

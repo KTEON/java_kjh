eb81bcf77b4b53598c8f30be1d1819638b2f792a
KTEON <104331435+KTEON@users.noreply.github.com>
2022-05-02 17:42:23 +0900

    자바 6일차 : 배열예제 - 배열 선언 예제

---
+++ java/src/day6/Ex3_Array_Declaration.java
@@ -0,0 +1,20 @@
+package day6;
+
+public class Ex3_Array_Declaration {
+	public static void main(String[] args) {
+		
+		int[] arr1;
+		int[] arr2 = null;		
+		// 5개짜리 정수 배열을 선언과 동시에 초기화
+		int[] arr3 = new int[5];
+		int size = 5;		
+		//5개짜리 정수 배열을 선언과 동시에 초기화
+		int[] arr4 = new int[size];		
+		//5개짜리 정수 배열을 선언과 동시에 초기화
+		int arr5[] = new int[5];
+		
+		int arr6[], arr7;// arr6 : 배열 arr7 : 변수
+		int []arr8, arr9;//arr 8 : 배열 arr9 : 배열
+
+	}
+}
